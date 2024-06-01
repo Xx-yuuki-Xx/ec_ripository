ec3eb39893b3028ae1acab864ddc80020c908366
Xx-yuuki-Xx <yuuki.hal1001@gmail.com>
2024-06-01 10:39:10 +0900

    [add]トップページへのlコントローラ　・　トップページhtml ・　トップページへのurlショートカット [fix]application.propertiesにport番号3333 ・　コンテキストパス /ecの追加 [delete] testパッケージの削除

---
+++ ec/src/main/java/ec/controller/IndexController.java
@@ -0,0 +1,14 @@
+package ec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	// トップページに遷移
+	@GetMapping(path = "/")
+	public String index() {
+		return "html/index";
+	}
+}
